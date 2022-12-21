06c57af3d6cb16306fb9980f3462cc70ef559130
vivekramkumar <41638630+vivekramkumar@users.noreply.github.com>
2022-12-21 11:04:16 +0530

    Add files via upload

--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css'
+import { BrowserRouter } from "react-router-dom";
 
+ReactDOM.render(
+  /* gives error for bootstrap modal pop-up if its in strict mode */
+  /*<React.StrictMode> */
+      <App />,
+  /* <React.StrictMode>, */
+
+  document.getElementById('root')
+  
+);
